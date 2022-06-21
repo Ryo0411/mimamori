5d883811a2cdf56161971c35e8d0d76ffa3008f0
Ryo0411 <watanabe@nippontect.co.jp>
2022-06-21 16:14:39 +0900

    一部ポップアップの追加②

--- resources/views/voice_walk.blade.php
+++ resources/views/voice_walk.blade.php
@@ -35,47 +35,58 @@
 	<section>
 		<div class="inner">
 			<div>
-			<x-alert type="success" :session="session('exe_msg')" />
-			<form method="POST" action="{{ route('voiceupdate') }}">
-				@csrf
-				<div class="block_txt mb2">
-					<p>マッチング精度向上のため声紋を登録します。</p>
+				<x-alert type="success" :session="session('exe_msg')" />
+				<form method="POST" action="{{ route('voiceupdate') }}">
+					@csrf
+					<div class="block_txt mb2">
+						<p>マッチング精度向上のため声紋を登録します。</p>
+					</div>
+
+					<input name="profile_id" type="hidden" id="profile_id" value="{{ @$wanderer_list->profile_id }}"></input>
+					<input name="voiceprint_flg" type="hidden" id="voiceprint_flg" value="{{ @$wanderer_list->voiceprint_flg }}"></input>
+					<input name="audio_file" type="hidden" id="audio_file" value=""></input>
+					<input name="voice_length" type="hidden" id="voice_length" value="{{ $voice_length }}"></input>
+
+					<div class="block_rec" id="voiceprint_btn">
+						<a id="exe_recording" class="btn_rec">
+							<img id="rec_img" src="{{ asset('img/rec_on.png') }}" class="img_rec" alt="録音">
+						</a>
+					</div>
+					<div class="btn_dl">
+						<a id="enrollmentDownload" class="soundsample"></a>
+					</div>
+
+					<div class="announce" id="result">録音ボタンをタップして、<br>表示される文章を読み上げて<br>音声を録音してください。</div>
+					@if ($errors->has('audio_file'))
+					<div class="alert alert-danger">
+						{{ "音声を録音してください。" }}</li>
+					</div>
+					@endif
+
+					<div>
+						<div style="margin-top: 20px"><label for="file" style="width: 100%; font-size: 16px">推奨学習</label></div>
+						<progress id="file" max="120000" value="{{ $voice_length }}" style="width: 100%; height: 30px"> 0% </progress>
+					</div>
+
+
+					<div class="btn mt2">
+						<button type="submit" id="btn_regist" class="btn-red" style="display: none;">登録</button>
+					</div>
+
+				</form>
+			</div>
+			<!-- 登録ポップアップ -->
+			<div id="recognition-result" title="タイトル" class="remodal" data-remodal-id="modal_e">
+				<h4>完了！</h4>
+				<div class="popup_inner">
+					<p id="result_pop">音声を追加学習しました。</p>
+					<div class="btn_popup">
+						<button data-remodal-action="close" class="remodal-confirm" id="okbtn">OK</button>
+						<button data-remodal-action="cancel" class="remodal-cancel" onclick="location.href='/home'">TOPへ</button>
+					</div>
 				</div>
-
-				<input name="profile_id" type="hidden" id="profile_id" value="{{ @$wanderer_list->profile_id }}"></input>
-				<input name="voiceprint_flg" type="hidden" id="voiceprint_flg" value="{{ @$wanderer_list->voiceprint_flg }}"></input>
-				<input name="audio_file" type="hidden" id="audio_file" value=""></input>
-				<input name="voice_length" type="hidden" id="voice_length" value="{{ $voice_length }}"></input>
-
-				<div class="block_rec" id="voiceprint_btn">
-					<a id="exe_recording" class="btn_rec">
-						<img id="rec_img" src="{{ asset('img/rec_on.png') }}" class="img_rec" alt="録音">
-					</a>
-				</div>
-				<div class="btn_dl">
-					<a id="enrollmentDownload" class="soundsample"></a>
-				</div>
-
-				<div class="announce" id="result">録音ボタンをタップして、<br>表示される文章を読み上げて<br>音声を録音してください。</div>
-                @if ($errors->has('audio_file'))
-                <div class="alert alert-danger">
-                    {{ "音声を録音してください。" }}</li>
-                </div>
-                @endif
-
-                <div>
-                    <div style="margin-top: 20px"><label for="file" style="width: 100%; font-size: 16px">推奨学習</label></div>
-                    <progress id="file" max="120000" value="{{ $voice_length }}" style="width: 100%; height: 30px"> 0% </progress>
-                </div>
-
-
-				<div class="btn mt2">
-					<button type="submit" id="btn_regist" class="btn-red" style="display: none;">登録</button>
-				</div>
-
-			</form>
+			</div>
 		</div>
-	</div>
 	</section>
 
 	<div id="text-recording" title="タイトル" class="remodal" data-remodal-id="modal_d" data-remodal-options="closeOnOutsideClick: false">
@@ -93,6 +104,17 @@
 		<div class="footer_ver">Ver. 1.0</div>
 		<div class="footer_copy">Provided by Nippontect Systems Co.,Ltd</div>
 	</footer>
+
+	<script>
+		/* bootstrap alertをx秒後に消す */
+		if (document.getElementById("alertfadeout") != null) {
+			location.href = '#modal_e';
+		}
+
+		document.getElementById("okbtn").onclick = function() {
+			window.setTimeout("$('#alertfadeout').fadeOut()", 1500);
+		};
+	</script>
 
 	<!-- Speech SDK reference sdk. -->
 	<!-- <script src="{{ asset('js/SpeechSDK/microsoft.cognitiveservices.speech.sdk.bundle.js') }}"></script> -->
